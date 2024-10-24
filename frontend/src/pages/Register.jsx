85718749151a8971a13287205e7ce173fdc239e1
Nithin1729S <sureshnithin1729@gmail.com>
2024-10-25 04:33:33 +0530

    auth completed

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,7 @@
+import Form from "../components/Form"
 
-export const Register = () => {
-  return (
-    <div>Register</div>
-  )
+function Register() {
+    return <Form route="/api/user/register/" method="register" />
 }
+
+export default Register
